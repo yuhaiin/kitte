ede0534f6ea4b433699dea8da37d42d179b5c320
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2024-10-13 19:25:15 +0800

    update

--- yuhaiin/get.sh
+++ yuhaiin/get.sh
@@ -4,7 +4,7 @@ CHINA_DOMAIN=https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/ma
 AD_WARS_HOSTS=https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts
 PGLYOYO="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblock&showintro=0&mimetype=plaintext"
 ANTI_AD_DOMAINS="https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/anti-ad-domains.txt"
-
+damengzhu_banad="https://raw.githubusercontent.com/damengzhu/banad/refs/heads/main/hosts.txt"
 
 curl ${CHINA_DOMAIN} | sed $'s/\r$//' | sed 's/server=\//\*\./g' | sed 's/\/114\.114\.114\.114//g' > accelerated-domains.china.conf
 curl ${GOOGLE_CHINA_DOMAIN} | sed $'s/\r$//' | sed 's/server=\///g' | sed 's/\/114\.114\.114\.114//g' > google.china.conf
@@ -13,4 +13,5 @@ curl ${APPLE_CHINA_DOMAIN} | sed $'s/\r$//' | sed 's/server=\///g' | sed 's/\/11
 
 curl "${ANTI_AD_DOMAINS}" | sed $'s/\r$//' | sed '/^#/d' | sed 's/#.*$//g' | sed 's/# /*\./g' | sed '/^ *$/d' | sed 's/^/*./g' > anti-ad-domains.txt
 curl "${PGLYOYO}" | sed $'s/\r$//' | sed '/ *[Adblock] */d' > pglyoyo.txt
-curl ${AD_WARS_HOSTS} | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed '/^#.*$/'d | sed '1,2d' | sed '/^ *$/d' > ad_wars_hosts
+curl ${AD_WARS_HOSTS} | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed 's/0.0.0.0 //g' | sed '/^#.*$/'d | sed '1,2d' | sed '/^ *$/d' > ad_wars_hosts
+curl ${damengzhu_banad} | sed $'s/\r$//' | sed 's/127.0.0.1 //g' | sed 's/0.0.0.0 //g' | sed '/^#.*$/'d | sed '1,2d' | sed '/^ *$/d' > damengzhu_banad
